dd8cb290ca2b74836412facd27fd1d1e37dcd769
FLAMEZOMBIE <ulzaparamarta@gmail.com>
2021-07-28 20:35:45 +0700

    Flutter-App

---
+++ lib/home.dart
@@ -0,0 +1,196 @@
+// Copyright 2018-present the Flutter authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import 'model/products_repository.dart';
+import 'model/product.dart';
+class HomePage extends StatelessWidget {
+  // TODO: Make a collection of cards (102)
+  // List<Card> _buildGridCards(int count){
+  //   List<Card> cards = List.generate(
+  //     count,
+  //       (int index) => Card(
+  //         clipBehavior: Clip.antiAlias,
+  //         child: Column(
+  //           crossAxisAlignment: CrossAxisAlignment.start,
+  //           children: <Widget> [
+  //             AspectRatio(
+  //                 aspectRatio: 18.0/11.0,
+  //               child: Image.asset('assets/diamond.png'),
+  //             ),
+  //             Padding(
+  //                 padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+  //               child: Column(
+  //                 crossAxisAlignment: CrossAxisAlignment.start,
+  //                 children: <Widget>[
+  //                   Text('Title'),
+  //                   SizedBox(height: 8.0),
+  //                   Text('Secondary Text'),
+  //                 ],
+  //               ),
+  //             )
+  //           ],
+  //         ),
+  //       )
+  //   );
+  //   return cards;
+  // }
+  List<Card> _buildGridCards(BuildContext context) {
+    List<Product> products = ProductsRepository.loadProducts(Category.all);
+
+    if (products == null || products.isEmpty) {
+      return const <Card>[];
+    }
+
+    final ThemeData theme = Theme.of(context);
+    final NumberFormat formatter = NumberFormat.simpleCurrency(
+        locale: Localizations.localeOf(context).toString());
+
+    return products.map((product) {
+      return Card(
+        clipBehavior: Clip.antiAlias,
+        // TODO: Adjust card heights (103)
+        elevation: 0.0,
+        child: Column(
+          // TODO: Center items on the card (103)
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            AspectRatio(
+              aspectRatio: 18 / 11,
+              child: Image.asset(
+                product.assetName,
+                package: product.assetPackage,
+                // TODO: Adjust the box size (102)
+                fit: BoxFit.fitWidth,
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                child: Column(
+                  // TODO: Align labels to the bottom and center (103)
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  // TODO: Change innermost Column (103)
+                  children: <Widget>[
+                    // TODO: Handle overflowing labels (103)
+                    Text(
+                      product == null ? '' : product.name,
+                      style: theme.textTheme.button,
+                      softWrap: false,
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 1,
+                    ),
+                    SizedBox(height: 4.0),
+                    Text(
+                      product == null ? '' : formatter.format(product.price),
+                      style: theme.textTheme.caption,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    }).toList();
+  }
+  // TODO: Add a variable for Category (104)
+  @override
+  Widget build(BuildContext context) {
+    // TODO: Return an AsymmetricView (104)
+    // TODO: Pass Category variable to AsymmetricView (104)
+    return Scaffold(
+      // TODO: Add app bar (102)
+      appBar: AppBar(
+        title: Text('SHRINE'),
+        leading: IconButton(
+          icon: Icon(
+            Icons.menu,
+            semanticLabel: 'menu',
+          ),
+          onPressed: () {
+            print('Menu button');
+          },
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.search,
+              semanticLabel: 'search',
+            ),
+            onPressed: (){
+              print('Search Button');
+            },
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.tune,
+              semanticLabel: 'filter',
+            ),
+            onPressed: (){
+              print('Filter Button');
+            },
+          )
+        ],
+      ),
+      // TODO: Add a grid view (102)
+      // body: GridView.count(
+      //   crossAxisCount: 2,
+      //   padding: EdgeInsets.all(16.0),
+      //   childAspectRatio: 8.0 / 9.0,
+      //   children: <Widget>[
+      //     Card(
+      //       clipBehavior: Clip.antiAlias,
+      //       child: Column(
+      //         crossAxisAlignment: CrossAxisAlignment.start,
+      //         children: <Widget>[
+      //           AspectRatio(aspectRatio: 18.0 / 11.0,
+      //             child: Image.asset('assets/diamond.png'),
+      //           ),
+      //           Padding(
+      //             padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+      //             child: Column(
+      //               crossAxisAlignment: CrossAxisAlignment.start,
+      //               children: <Widget>[
+      //                 Text('Title'),
+      //                 SizedBox(height: 8.0),
+      //                 Text('Secondary Text')
+      //               ],
+      //             ),
+      //           )
+      //         ],
+      //       ),
+      //     )
+      //   ],
+      // ),
+      // body: GridView.count(
+      //   crossAxisCount: 2,
+      //   padding: EdgeInsets.all(16.0),
+      //   childAspectRatio: 8.0/9.0,
+      //   children: _buildGridCards(10),
+      // ),
+      body: GridView.count(
+          crossAxisCount: 2,
+          padding: EdgeInsets.all(16.0),
+          childAspectRatio: 8.0 / 9.0,
+          children: _buildGridCards(context) // Changed code
+      ),
+      // TODO: Set resizeToAvoidBottomInset (101)
+      resizeToAvoidBottomInset: false,
+    );
+  }
+}
---
+++ lib/login.dart
@@ -0,0 +1,138 @@
+// Copyright 2018-present the Flutter authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  // TODO: Add text editing controllers (101)
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _unfocusedColor = Colors.grey[600];
+  final _usernameFocusNode = FocusNode();
+  final _passwordFocusNode = FocusNode();
+  @override
+  void initState() {
+    super.initState();
+    _usernameFocusNode.addListener(() {
+      setState(() {
+        //Redraw so that the username label reflects the focus state
+      });
+    });
+    _passwordFocusNode.addListener(() {
+      setState(() {
+        //Redraw so that the password label reflects the focus state
+      });
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          children: <Widget>[
+            SizedBox(height: 80.0),
+            Column(
+              children: <Widget>[
+                Image.asset('assets/diamond.png'),
+                SizedBox(height: 16.0),
+                Text('SHRINE',
+                style: Theme.of(context).textTheme.headline5,),
+              ],
+            ),
+            SizedBox(height: 120.0),
+            // TODO: Wrap Username with AccentColorOverride (103)
+            // TODO: Remove filled: true values (103)
+            // TextField(
+            //   controller: _usernameController,
+            //   decoration: InputDecoration(
+            //     // Removed filled: true
+            //     labelText: 'Username',
+            //     labelStyle: TextStyle(color: Theme.of(context).accentColor),
+            //   ),
+            // ),
+            // SizedBox(height: 12.0),
+            // TextField(
+            //   controller: _passwordController,
+            //   decoration: InputDecoration(
+            //     // Removed filled: true
+            //     labelText: 'Password',
+            //     labelStyle: TextStyle(color: Theme.of(context).accentColor),
+            //   ),
+            //   obscureText: true,
+            // ),
+            // TODO: Wrap Password with AccentColorOverride (103)
+            // TODO: Add TextField widgets (101)
+            // [Name]
+            TextField(
+              controller: _usernameController,
+              decoration: InputDecoration(
+                labelText: 'Username',
+                labelStyle: TextStyle(
+                    color: _usernameFocusNode.hasFocus
+                        ? Theme.of(context).accentColor
+                        : _unfocusedColor),
+              ),
+              focusNode: _usernameFocusNode,
+            ),
+            SizedBox(height: 12.0),
+            TextField(
+              controller: _passwordController,
+              decoration: InputDecoration(
+                labelText: 'Password',
+                labelStyle: TextStyle(
+                    color: _passwordFocusNode.hasFocus
+                        ? Theme.of(context).accentColor
+                        : _unfocusedColor),
+              ),
+              focusNode: _passwordFocusNode,
+            ),
+            // TODO: Add button bar (101)
+            ButtonBar(
+              children: <Widget>[
+                FlatButton(
+                  child: Text('CANCEL'),
+                  shape: BeveledRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(7.0)),
+                  ),
+                  onPressed: (){
+                    _usernameController.clear();
+                    _passwordController.clear();
+                  },
+                ),
+                RaisedButton(
+                  child: Text('NEXT'),
+                  elevation: 8.0, // New code
+                  shape: BeveledRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(7.0)),
+                  ),
+                  onPressed: (){
+                    Navigator.pop(context);
+                  },
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// TODO: Add AccentColorOverride (103)
